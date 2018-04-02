38a4847b9cf7543470005d86f28d3b8469d4622e
Basilio Cáceres <basilio.caceres@gmail.com>
2018-04-01 23:02:34 -0300

    registro: sexo

---
+++ src/components/RegistrySex.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="screen bg-main justify-content-between">
+    <header class="header container">
+      <button-prev></button-prev>
+    </header>
+    <div class="main container">
+      <h2 class="mb-3">Indica tu sexo</h2>
+      <form class="row">
+        <div class="col-6 pr-2">
+          <button-sex :name="'sex'" :defaultValue="'male'" :value="user.sex" @change="changeValue">Hombre</button-sex>
+        </div>
+        <div class="col-6 pl-2">
+          <button-sex :name="'sex'" :defaultValue="'female'" :value="user.sex" @change="changeValue">Mujer</button-sex>
+        </div>
+      </form>
+    </div>
+    <footer class="footer container">
+      <button-audio></button-audio>
+      <button-next :linkTo="'/registry-job'" :isDisabled="!canContinue"></button-next>
+    </footer>
+  </div>
+</template>
+
+<script>
+import ButtonAudio from './parts/ButtonAudio'
+import ButtonPrev from './parts/ButtonPrev'
+import ButtonNext from './parts/ButtonNext'
+import ButtonSex from './parts/ButtonSex'
+
+export default {
+  name: 'RegistrySex',
+  components: {
+    ButtonAudio,
+    ButtonPrev,
+    ButtonNext,
+    ButtonSex
+  },
+  data: function () {
+    return {
+      user: {
+        sex: ''
+      }
+    }
+  },
+  methods: {
+    changeValue: function (newValue) {
+      this.user.sex = newValue
+    }
+  },
+  computed: {
+    canContinue: function () {
+      return this.user.sex !== ''
+    }
+  }
+}
+</script>
---
+++ src/components/parts/ButtonSex.vue
@@ -0,0 +1,86 @@
+<!--
+Ejemplo obtenido de: https://jsfiddle.net/3uznmk72/154/
+-->
+<template>
+  <label class="button-sex">
+    <input type="radio" name="sex" :value="defaultValue" v-model="buttonValue">
+    <span class="wrapper">
+      <span class="icon">
+        <svgicon :name="icon" width="22" height="50" color="#fff"></svgicon>
+      </span>
+      <span class="text">
+        <slot></slot>
+      </span>
+    </span>
+  </label>
+</template>
+
+<script>
+import '@/assets/icons/man'
+import '@/assets/icons/woman'
+
+export default {
+  name: 'ButtonSex',
+  props: ['value', 'defaultValue'],
+  computed: {
+    buttonValue: {
+      get: function () {
+        return this.value
+      },
+      set: function () {
+        this.$emit('change', this.defaultValue)
+      }
+    },
+    icon: function () {
+      return this.defaultValue === 'male' ? 'man' : 'woman'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "./../../assets/sass/main.scss";
+
+.button-sex {
+  display: block;
+  & .wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    color: $gray;
+    background-color: rgba($white, .2);
+    transition: $transition-base;
+  }
+  & .icon,
+  & .text {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    transition: $transition-base;
+  }
+  & .icon {
+    height: 11rem;
+  }
+  & .text {
+    height: 70px;
+    font-size: $font-size-base;
+    background-color: $white;
+  }
+  & input[type="radio"] {
+    position: absolute;
+    z-index: -1;
+    opacity: 0;
+  }
+  & input[type="radio"]:focus ~ .wrapper {
+    box-shadow: $input-btn-focus-box-shadow;
+  }
+  & input[type="radio"]:checked ~ .wrapper {
+    background-color: $brand-green;
+  }
+  & input[type="radio"]:checked ~ .wrapper .text {
+    box-shadow: inset 0 -5px 0 0 $brand-green;
+  }
+}
+</style>
